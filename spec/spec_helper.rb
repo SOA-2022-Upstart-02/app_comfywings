24c8017a211b1aca8004107aa72ed78e3b297810
javaku9527 <smog3719@gmail.com>
2022-10-21 00:31:20 +0800

    add test converage in project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
-# TODO: add/move required libraries, constants, and files required for tests 
+require 'simplecov'
+SimpleCov.start
+
+require 'yaml'
+
+require 'minitest/autorun'
+
+require_relative '../lib/amadeus_api'
+
+# TODO: add/move required libraries, constants, and files required for tests
 #       when you write or create a new test
+CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
+ANADEUS_TOKEN = CONFIG['AMADEUS_KEY']
+ANADEUS_SECRET = CONFIG['AMADEUS_SECRET']
+
+CORRECT = YAML.safe_load(File.read('spec/fixtures/flight_results.yml'))
