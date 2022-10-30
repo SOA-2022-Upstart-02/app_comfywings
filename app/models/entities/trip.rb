51bbd31f119b85ec8f475f6b2397aa6e7d241b44
javaku9527 <smog3719@gmail.com>
2022-10-30 18:07:44 +0800

    finish datamapper

---
+++ app/models/entities/trip.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: false
+
+require 'dry-struct'
+require 'dry-types'
+
+require_relative 'flight'
+
+module ComfyWings
+  module Entity
+    class Trip < Dry::Struct
+      include Dry.Types
+
+      attribute :origin,        Strict::String
+      attribute :destination,   Strict::String
+      attribute :duration,      Strict::String
+      attribute :price,         Strict::String
+      attribute :flights,       Strict::Array.of(Flight)
+    end
+  end
+end
--- spec/gateway_amadeus_api_spec.rb
+++ spec/gateway_amadeus_api_spec.rb
@@ -27,47 +27,9 @@ describe 'Tests AMADEUS API library' do
 
   describe 'Flight information' do
     it 'HAPPY: should provide correct flight attributes' do
-      matched_flights = ComfyWings::Amadeus::Api.new(AMADEUS_KEY, AMADEUS_SECRET)
-                                           .flight('TPE', 'MAD', '2022-11-01', '2022-11-05')
+      matched_flights = ComfyWings::Amadeus::TripMapper.new(AMADEUS_KEY, AMADEUS_SECRET)
+                                                       .search('TPE', 'MAD', '2022-11-01', '2022-11-05')
       _(matched_flights.size).must_equal CORRECT['flight_num']
-    end
-
-    it 'SAD: should raise exception on incorrect flight information' do
-      _(proc do
-          ComfyWings::Amadeus::Api.new('BAD_TOKEN', 'BAD_SECRET')
-                             .flight('AIRPORT_NOT_EXIST', 'AIRPORT_NOT_EXIST', '2022-11-01', '2022-11-05')
-        end).must_raise ComfyWings::Amadeus::Api::Response::Unauthorized
-    end
-
-    it 'SAD: should raise exception when unauthorized' do
-      _(proc do
-        ComfyWings::Amadeus::Api.new('BAD_TOKEN', 'BAD_SECRET')
-                           .flight('TPE', 'MAD', '2022-11-01', '2022-11-05')
-      end).must_raise ComfyWings::Amadeus::Api::Response::Unauthorized
-    end
-  end
-
-  describe 'Flight information' do
-    before do
-      @matched_flights = ComfyWings::Amadeus::Api.new(AMADEUS_KEY, AMADEUS_SECRET)
-                                            .flight('TPE', 'MAD', '2022-11-01', '2022-11-05')
-    end
-
-    it 'HAPPY: should recognize flight' do
-      _(@matched_flights.flights[0]).must_be_kind_of ComfyWings::Amadeus::Flight
-    end
-
-    it 'HAPPY: should identify flight' do
-      _(@matched_flights.flights[0].total_price).must_equal CORRECT['flights'][0]['total_price']
-    end
-  end
-
-  describe 'Token generator' do
-    before do
-      @token = AuthToken.new('config/secrets.yml')
-    end
-    it 'It should create a new auth token' do
-      _(@token.obtain_token).wont_be_nil
     end
   end
 end
