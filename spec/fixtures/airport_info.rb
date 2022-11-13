6135cf313e9bc0969f63828f44d5205265d47b3d
Chiuzu-chilumbu <chiuzubenny@hotmail.com>
2022-11-13 08:50:23 +0800

    updated airport info file

--- spec/fixtures/airport_info.rb
+++ spec/fixtures/airport_info.rb
@@ -3,13 +3,68 @@
 require 'http'
 require 'json'
 require 'yaml'
-require_relative 'flight_info'
-require_relative '../../app/models/gateways/amadeus_api'
+require_relative 'auth_token'
 
 # airport token
-AuthToken.new
+NEW_TOKEN = AuthToken.new
+DEPATURE = 'TPE'
 
-# TODO: the following
-# Need depature code to obtain airport information
-# example https://test.api.amadeus.com/v1/airport/direct-destinations?departureAirportCode=MAD&max=2
-# try using current TPE code
+module ComfyWings
+  # obtains data from the amadeus api
+  class AirportMapper
+    def initialize(api_key, api_secret, gateway_class = Amadeus::Api)
+      @api_key = api_key
+      @api_secret = api_secret
+      @gateway_class = gateway_class
+      @gateway = @gateway_class.new(@api_key, @api_secret)
+    end
+
+    # def airport_data(departure)
+    #   @gateway.airport_data(departure).map do |data|
+    #     AirportMapper.build_entity(data)
+    #   end
+    # end
+
+    def airport_size(departure)
+      @gateway.airport_size(departure)
+    end
+
+    def airport_data(departure)
+      @gateway.airport_data(departure)
+    end
+
+    def self.build_entity(data)
+      DataMapper.new(data).build_entity
+    end
+
+    # obtain aiport infomation from hash
+    class DataMapper
+      def initialize(hash_data)
+        @data = hash_data
+      end
+
+      def build_entity
+        Entity::Airport.new(
+          type: nil,
+          subtype:,
+          name:,
+          iata_code:
+        )
+      end
+
+      def subtype
+        @data['subtype']
+      end
+
+      def name
+        @data['name']
+      end
+
+      def iata_code
+        @data['iataCode']
+      end
+    end
+  end
+end
+
+# File.write('spec/fixtures/airport_results.yml', airport_results.to_yaml)
