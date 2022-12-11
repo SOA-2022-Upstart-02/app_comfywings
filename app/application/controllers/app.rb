c70f2de7af18d50c49eeec1ce0326dd5efadabb7
Chiuzu-chilumbu <chiuzubenny@hotmail.com>
2022-12-11 17:50:28 +0800

    standard commit

--- app/application/controllers/app.rb
+++ app/application/controllers/app.rb
@@ -18,48 +18,18 @@ module ComfyWings
                     css: 'style.css'
     plugin :common_logger, $stderr
 
-    route do |routing| # rubocop:disable Metrics/BlockLength
+    route do |routing|
       routing.assets # load CSS
       response['Content-Type'] = 'text/html; charset=utf-8'
 
       # GET /
       routing.root do
-        currency_list = Repository::For.klass(Entity::Currency).all
-        view 'home', locals: { currencies: currency_list }
+        view 'home', locals: {}
       end
 
-      routing.is 'flight' do
-        # POST /flight
-        routing.post do
-          trip_request = Forms::NewTripQuery.new.call(routing.params)
-          trips = Service::FindTrips.new.call(trip_request)
-          if trips.failure?
-            flash[:error] = trips.failure
-            response.status = 400
-            routing.redirect '/'
-          end
-          view 'flight', locals:
-          {
-            trips: trips.value!,
-            trip_request: trip_request.values
-          }
-        end
-      end
-
-      routing.is 'airport' do
+      routing.on 'airport' do
         # GET /airports
-        routing.is do
-          first_airport = Repository::For.klass(Entity::Airport).first
-          view 'airport', locals: { airport: first_airport }
-        end
-        #  TODO: fix code to ensure all airports are obtained through search
-        # routing.is aiport_search do
-        # routing.post do
-        #  airport_code = Forms::SearchAirport.new.call(routing.params)
-        #  airport = Service::FindAirports.new.call(airport_code)
-        # view 'airport_search', locals: { airport_info: airport.value! }
-        # end
-        #  end
+        view 'airport', locals: {}
       end
     end
   end
---
+++ app/presentation/view_objects/airports.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Views
+  # class for trip info
+  class Airport
+    def initialize(iata_code)
+      @org_iata_code = iata_code
+    end
+
+    def airport_name
+      @org_iata_code.last
+    end
+
+    def city_airport_name
+      @org_iata_code.first
+    end
+
+    def country
+      @org_iata_code
+    end
+
+    def iata_code
+      @org_iata_code
+    end
+  end
+end
